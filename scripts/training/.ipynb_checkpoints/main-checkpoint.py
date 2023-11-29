da730fe3b22ec192122ac07b6cca5e43d38a8766
yonny <ycnovac@unal.edu.co>
2023-11-28 22:50:04 -0500

    adding basic regresion model

--- scripts/training/.ipynb_checkpoints/main-checkpoint.py
+++ scripts/training/.ipynb_checkpoints/main-checkpoint.py
@@ -3,8 +3,6 @@ import numpy as np
 import seaborn as sns
 import matplotlib.pyplot as plt
 import plotly.express as px
-from sklearn.linear_model import LinearRegression
-from sklearn.model_selection import train_test_split
 
 df = pd.read_csv('../../src/nombre_paquete/database/database.csv')
 
@@ -66,30 +64,6 @@ fig = px.scatter_geo(df, lat='Latitude', lon='Longitude', hover_name='Magnitude'
 fig.update_layout(title='Terremotos en los periodos, 1965-2016', title_x=0.5)
 fig.show()
 
-# codificacion de las variables categoricas
 
-df.head()
-
-df=df.drop(["Date", "Time"], axis=1)
-
-df['Type']=pd.factorize(df['Type'])[0]
-
-df['Magnitude Type']=pd.factorize(df['Magnitude Type'])[0]
-
-df['Status']=pd.factorize(df['Status'])[0]
-
-# Probando un modelo de regresion lineal
-
-X, y = df.drop('Magnitude', axis=1), df['Magnitude']
-
-X_train, X_test, y_train, y_test = train_test_split(X,y, test_size=0.3)
-
-reg = LinearRegression()
-
-model = reg.fit(X_train, y_train)
-
-reg.score(X_test, y_test)
-
-# Probando una red neuronal profunda
 
 
